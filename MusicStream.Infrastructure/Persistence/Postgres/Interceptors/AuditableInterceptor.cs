a7f1d00e86c0d5b2cb8717e8c6bce83895f00a57
Reza <rezajafarpoor222@gmail.com>
2025-08-18 20:56:40 +0330

    Audit interceptor added

---
+++ MusicStream.Infrastructure/Persistence/Postgres/Interceptors/AuditableInterceptor.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using MusicStream.Domain.Common;
+
+namespace MusicStream.Infrastructure.Persistence.Postgres.Interceptors;
+
+public class AuditableInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        Audit(eventData);
+        return base.SavingChanges(eventData, result);
+    }
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        Audit(eventData);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+
+    private void Audit(DbContextEventData eventData)
+    {
+        var entries = eventData.Context?.ChangeTracker.Entries<Auditable>();
+        if (entries is not null)
+        {
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+
+}
